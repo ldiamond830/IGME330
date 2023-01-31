ee348abe86044703e9b87a7e9ea7345c463c8165
underscore830 <70597619+underscore830@users.noreply.github.com>
2023-01-31 16:27:02 -0500

    added print five button

--- HW/diamond-l-HW01/src/main.js
+++ HW/diamond-l-HW01/src/main.js
@@ -1,5 +1,5 @@
 
-	"use strict";
+	
 	
 	const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
 	
@@ -8,7 +8,7 @@
 	const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
 
 
-    window.onload = init;
+   
 
     const generateTechno = (num) =>{
         
@@ -30,7 +30,7 @@
          
          document.querySelector("#output").innerHTML = completeTechno; 
          
-        console.log("call");
+        //console.log("call");
     }
 
     const generateOne = () => generateTechno(1);
@@ -39,18 +39,21 @@
     const generateFive = () => generateTechno(5);
 
     const init = () =>{
-        //get references to button
+        //get references to buttons
         const printOneButton = document.querySelector("#print-one-button");
-
+        const printFiveButton = document.querySelector("#print-five-button");
         //calls print babble intially to have some starting text
         generateOne();
         //attaches printBable to the button's click event
         printOneButton.onclick = generateOne;
+        printFiveButton.onclick = generateFive;
 
        
 
 
     }
+
+    window.onload = init;
 
     
 	
