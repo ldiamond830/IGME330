681cdb3a0331aef8c43edc1a3c286bad0e365d86
Lucas Diamond (RIT Student) <lgd1649@rit.edu>
2023-02-23 17:13:32 -0500

    more work on audio visualizer

--- HW/web-audio-hw/src/audio.js
+++ HW/web-audio-hw/src/audio.js
@@ -15,25 +15,27 @@ const DEFAULTS = Object.freeze({
 
 // 4 - create a new array of 8-bit integers (0-255)
 // this is a typed array to hold the audio frequency data
+let audioData = new Uint8Array(DEFAULTS.numSamples/2);
 
 
 // **Next are "public" methods - we are going to export all of these at the bottom of this file**
-
+function setupWebaudio(filePath){
 // 1 - The || is because WebAudio has not been standardized across browsers yet
-
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+audioCtx = new AudioContext();
 
 // 2 - this creates an <audio> element
-
+element = new Audio();
 
 // 3 - have it point at a sound file
-
+loadSoundFile(filePath);
 
 // 4 - create an a source node that points at the <audio> element
-
+sourceNode = audioCtx.createMediaElementSource(element);
 
 // 5 - create an analyser node
 // note the UK spelling of "Analyser"
-
+analyserNode = audioCtx.createAnalyser();
 /*
 // 6
 We will request DEFAULTS.numSamples number of samples or "bins" spaced equally 
@@ -45,11 +47,33 @@ the amplitude of that frequency.
 */ 
 
 // fft stands for Fast Fourier Transform
-
+Animation;yserMpde.fftSize = DEFAULTS.numSamples;
 
 // 7 - create a gain (volume) node
-
+gainNode = audioCtx.createGain();
+gainNode.gain.value = DEFAULTS.gain;
 
 // 8 - connect the nodes - we now have an audio graph
+sourceNode.connect(analyserNode);
+analyserNode.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+}
 
-// make sure that it's a Number rather than a String
+function loadSoundFile(filePath){
+    element.src = filePath;
+}
+
+function playCurrentSound(){
+    element.play();
+}
+
+function pauseCurrentSound(){
+    element.pause();
+}
+
+function setVolume(value){
+    value = Number(value);// make sure that it's a Number rather than a String
+    gainNode.gain.value = value;
+}
+
+export {audioCtx,setupWebaudio,playCurrentSound,pauseCurrentSound,loadSoundFile,setVolume,analyserNode};
